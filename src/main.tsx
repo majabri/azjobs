5fc104488245260d299f7ee3bcdfec9f48891a11
majabri <majabri@users.noreply.github.com>
2026-04-13 01:55:16 -0400

    Wrap App component in ThemeProvider and StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,15 @@
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "@/context/ThemeContext";
+import "./theme.css";
 import App from "./shell/App.tsx";
 import "./index.css";
 import "./lib/i18n";
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
