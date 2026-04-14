c6697ead270f5150e2e47efb741087473d7730cc
majabri <majabri@users.noreply.github.com>
2026-04-13 23:23:04 -0400

    fix: remove duplicate ThemeProvider from main.tsx to fix crash

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./shell/App.tsx";
-import { ThemeProvider } from "./contexts/ThemeContext";
 import "./index.css";
 import "./theme.css";
 import "./lib/i18n";
 
-createRoot(document.getElementById("root")!).render(
-  <ThemeProvider>
-    <App />
-  </ThemeProvider>
-);
+createRoot(document.getElementById("root")!).render(<App />);
